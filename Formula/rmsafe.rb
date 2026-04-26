2047bfadf39a9e7fbaed672bb966100cf611dae1
leftrk <tulingjiaoyu@gmail.com>
2026-04-26 18:48:42 +0800

    fix: use Language::Python::Virtualenv pattern

--- Formula/rmsafe.rb
+++ Formula/rmsafe.rb
@@ -1,16 +1,27 @@
 class Rmsafe < Formula
+  include Language::Python::Virtualenv
+
   desc "Modern rm replacement with trash, undo, and Btrfs support"
   homepage "https://github.com/leftrk/rmsafe"
-  license "GPL-2.0-only"
   url "https://files.pythonhosted.org/packages/source/r/rmsafe/rmsafe-1.1.1.tar.gz"
   sha256 "aef331dc6c072d55ed05fe9181b4b1dc2efdf7b1b3c3d416bdf1f5030f67ab6a"
+  license "GPL-2.0-only"
   head "https://github.com/leftrk/rmsafe.git", branch: "main"
 
   depends_on "python@3.11"
 
+  resource "typer" do
+    url "https://files.pythonhosted.org/packages/source/t/typer/typer-0.25.0.tar.gz"
+    sha256 "123eaf9f19bb40fd268310e12a542c0c6b4fab9c98d9d23342a01ff95e3ce930"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/source/r/rich/rich-15.0.0.tar.gz"
+    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
+  end
+
   def install
-    venv = virtualenv_create(libexec, "python3.11")
-    venv.pip_install_and_link(buildpath)
+    virtualenv_install_with_resources
   end
 
   def caveats
