6db7adca56de2e8f0277e99064c0ac32f2ecaa7b
Guan Hua <tulingjiaoyu@gmail.com>
2026-05-28 00:23:24 +0800

    wordroot 0.1.1: macOS 13+ 支持

--- Casks/wordroot.rb
+++ Casks/wordroot.rb
@@ -1,8 +1,8 @@
 cask "wordroot" do
-  version "0.1.0"
-  sha256 "43305a80e593f97d0e19deb849a90681351e9e444282c1c7367e99e6bb86049b"
+  version "0.1.1"
+  sha256 "b5ffba7f78bfa4ccd3d223d9fede6870e4242db4e64d614de99d64f4ed5e8f21"
 
-  url "https://github.com/leftrk/word-root/releases/download/v0.1.0/Wordroot.zip"
+  url "https://github.com/leftrk/word-root/releases/download/v0.1.1/Wordroot-0.1.1.zip"
   name "Wordroot"
   desc "词根词典，让词汇有迹可循"
   homepage "https://github.com/leftrk/word-root"
