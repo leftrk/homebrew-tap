40021210c91304f3f2f51e2b0fa5512e287bc307
leftrk <hua.guan@jaco.live>
2026-05-28 01:22:45 +0800

    wordroot: 0.1.2 -> 0.1.3 (fix macOS 13 crash)

--- Casks/wordroot.rb
+++ Casks/wordroot.rb
@@ -1,6 +1,6 @@
 cask "wordroot" do
-  version "0.1.2"
-  sha256 "c527f9c6bbff36f80496a79fc8c0fa85532702ca12603b44b9f41cd28113a1ee"
+  version "0.1.3"
+  sha256 "80778475baf163d17ef247242e47199118a07d17fb7455678dff6dfa29d4a00e"
 
   url "https://github.com/leftrk/wordroot-dist/releases/download/v#{version}/Wordroot-#{version}.dmg"
   name "Wordroot"
